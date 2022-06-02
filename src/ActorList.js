688824c6e21e797e220cd7caa60ef35172f82e41
Nouar-git <81237818+Nouar-git@users.noreply.github.com>
2022-06-02 23:30:57 +0200

    Updaterat ActorList.js

--- src/ActorList.js
+++ src/ActorList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ActorInfo from "./ActorInfo";
 
 export default function ActorList(props) {
     return (
@@ -7,14 +6,13 @@ export default function ActorList(props) {
             <h1 className="text-center mb-5">Most popular actors and actresses</h1>
             <div className="row g-2">
                 {props.actors.map( actor => (
-                    <div key={actor.id} className="shadow col-6 col-md-3 mb-3">
-                        <img src={'https://image.tmdb.org/t/p/w235_and_h235_face' + actor.profile_path} className="card-img-top" alt={actor.name}></img>
+                    <button key={actor.id} onClick={(e) => { props.getActor(e, actor.id, actor) }} className="shadow col-6 col-md-3 mb-3 border-0 bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img src={'https://image.tmdb.org/t/p/w235_and_h235_face' + actor.profile_path} className="card-img-top" alt={actor.name}></img>
                         <h5 className="card-title">{actor.name}</h5>
-                    </div>
+                    </button>
                 ))}
 
             </div>
         </main>
     )
 }
-
