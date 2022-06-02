cd9459ad1a6b2efebcd8cb76bdd691715ce7cd60
Nouar-git <81237818+Nouar-git@users.noreply.github.com>
2022-06-02 23:31:56 +0200

    Updaterat App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ActorInfo from "./ActorInfo";
 
 function App() {
   const [actors, setActors] = useState([]);
+  const [show, setShow] = useState({});
 
   useEffect ( () => {
       const mostPopularActors = async () => {
@@ -21,12 +22,33 @@ function App() {
 
       mostPopularActors();
     }, []);
+    
+    async function getActor(e , id, actor) {
+      e.preventDefault();
+      const apiKey = '9595bac57a62b5ff9e1564b3b85150f5';
+      const url = 'https://api.themoviedb.org/3/person/' + id + '?api_key=' + apiKey;
+  
+      const response = await fetch(url);
+      const details = await response.json();
+  
+      if (details && actor) {
+        setShow({
+          name : details.name,
+          biography : details.biography,
+          birthday : details.birthday,
+          image : `https://image.tmdb.org/t/p/w600_and_h900_bestv2${details.profile_path}`,
+          known_for : actor.known_for
+  
+        });
+      }
+    }
 
   return (
     <div className="container">
-      <ActorList actors={actors} />
-      <ActorInfo />
+      <ActorList actors={actors} getActor={getActor}/>
+      <ActorInfo show={show} />
     </div>
+    
   );
 }
 
