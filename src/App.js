294db641bbb3d03d83c2854be132b56bd98c088b
Nouar-git <81237818+Nouar-git@users.noreply.github.com>
2022-06-04 00:03:05 +0200

    fixsade konflikter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect} from "react";
 import ActorList from "./ActorList";
 import ActorInfo from "./ActorInfo";
@@ -57,28 +56,3 @@ function App() {
 }
 
 export default App;
-=======
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Favorites from './components/Favorites';
-
-
-const App =()=>{
-  return (
-    <Router>
-      <div className='App'>
-        <Navbar />   
-        <div className='container'>
-            <Routes>
-              <Route exact path='/' element={<Home/>} />
-              <Route exact path='/favorites' element={<Favorites/>} />
-            </Routes>
-         </div>
-      </div>
-    </Router>
-)};
-
-export default App;
->>>>>>> fa0316334134fb53d5892357858671af184fc766
--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fa0316334134fb53d5892357858671af184fc766
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-
-=======
->>>>>>> fa0316334134fb53d5892357858671af184fc766
