f64b9b7540785984492230e1c683b3f2bf4569eb
Nouar-git <81237818+Nouar-git@users.noreply.github.com>
2022-06-14 11:17:36 +0200

    Uppdaterat compnents App.js, ActorList.js och ActorInfo.js och lyckades hämta movies från Apiet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import ActorList from "./ActorList";
 import ActorInfo from "./ActorInfo";
 import Footer from "./Footer";
 
-
-
 function App() {
   const [actors, setActors] = useState([]);
-  const [show, setShow] = useState({});
+  const [actor, setActor] = useState({});
+
 
   useEffect ( () => {
       const mostPopularActors = async () => {
@@ -25,22 +24,33 @@ function App() {
       mostPopularActors();
     }, []);
     
-    async function getActor(e , id, actor) {
+    async function getActor(e , id) {
       e.preventDefault();
       const apiKey = '9595bac57a62b5ff9e1564b3b85150f5';
-      const url = 'https://api.themoviedb.org/3/person/' + id + '?api_key=' + apiKey;
-  
+      
+      const url = `https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}`;
       const response = await fetch(url);
       const details = await response.json();
+
+
+      const urlMovies = `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${apiKey}`;
+      const responsemovies = await fetch(urlMovies);
+      const movies = await responsemovies.json();
+
   
-      if (details && actor) {
-        setShow({
+      
+      if (details && movies) {
+        const moviesArr = [];
+        movies.cast.map( movie =>
+          moviesArr.push(`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`)
+        );
+
+        setActor({
           name : details.name,
           biography : details.biography,
           birthday : details.birthday,
+          moviesArr : moviesArr,
           image : `https://image.tmdb.org/t/p/w600_and_h900_bestv2${details.profile_path}`,
-          known_for : actor.known_for
-  
         });
       }
     }
@@ -48,7 +58,7 @@ function App() {
   return (
     <div className="container">
       <ActorList actors={actors} getActor={getActor}/>
-      <ActorInfo show={show} />
+      <ActorInfo actor={actor} />
       <Footer />
     </div>
     
