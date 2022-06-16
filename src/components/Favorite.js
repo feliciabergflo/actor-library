70b7fc1b7cec9f66abe826ff8f5fa0e76f95237c
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2022-06-16 17:56:48 +0200

    ff

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function Favorite(props) {
     return (
         <li className="list-group-item">
-            { props.item.name }
+            <Link to='/' className="link-dark">{ props.item.name }</Link>
             <button className="btn-close float-end" onClick={() => {props.deleteFavorite(props.item.id)}}></button>
         </li>
     )
